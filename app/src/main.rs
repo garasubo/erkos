5706e35fa48123d68fe36ccbc52a11dfd6dc595e
garasubo <garasubo@gmail.com>
2019-05-03 16:40:01 +0900

    create scheduler

--- app/src/main.rs
+++ app/src/main.rs
@@ -7,10 +7,11 @@ use rt::Vector;
 use rt::SYSCALL_FIRED;
 use cortex_m_semihosting::{debug, hio::{self, HStdout}};
 use log::{debug, Log, dhprintln};
+use core::option::Option;
 use core::slice::from_raw_parts;
 use device::gpio::Gpio;
 use device::rcc::{Rcc, RCC};
-use device::serial::Serial;
+use device::serial::{Usart, Serial};
 use device::irqs::IrqId;
 use device::IRQS;
 use device::nvic::Nvic;
@@ -52,6 +53,114 @@ macro_rules! static_init {
     }
 }
 
+struct ProcessList<'a> {
+    current: Process<'a>,
+    next: Option<&'a mut ProcessList<'a>>,
+}
+
+impl<'a> ProcessList<'a> {
+    fn create(process: Process<'a>) -> ProcessList<'a> {
+        ProcessList {
+            current: process,
+            next: None,
+        }
+    }
+}
+
+struct Scheduler<'a> {
+    procs: Option<&'a mut ProcessList<'a>>,
+    last: Option<*mut ProcessList<'a>>,
+}
+
+impl<'a> Scheduler<'a> {
+    fn create() -> Scheduler<'a> {
+        Scheduler {
+            procs: None,
+            last: None,
+        }
+    }
+
+    pub fn push(&mut self, proc: &'a mut ProcessList<'a>) {
+        if self.last.is_none() {
+            let proc_ptr = proc as *mut ProcessList;
+            self.procs.replace(proc);
+            self.last.replace(proc_ptr);
+        } else {
+            let last_ptr = self.last.unwrap();
+            let proc_ptr = proc as *mut ProcessList;
+            self.last.replace(proc_ptr);
+            unsafe { (*last_ptr).next.replace(proc); }
+        }
+    }
+
+    pub fn run(&mut self, serial: &mut Serial<Usart>, nvic: &Nvic) {
+        while self.procs.is_none() {
+            unsafe { asm!("wfi"::::"volatile"); }
+        }
+
+
+        match self.procs.iter_mut().next() {
+            Some(current_item) => {
+
+                let process = &mut (current_item.current);
+                process.execute();
+                unsafe {
+                    if SYSCALL_FIRED > 0 {
+                        let base_frame = from_raw_parts(process.sp as *const u32, 8);
+                        let svc_id = base_frame[0];
+                        if svc_id == 1 {
+                            let arg2 = base_frame[2] as usize;
+                            let arg1 = from_raw_parts(base_frame[1] as *const u8, arg2);
+
+                            for i in 0..arg2 {
+                                serial.write(arg1[i] as char);
+                            }
+                        } else if svc_id == 2 {
+                            SYSTICK_FIRED = 1;
+                        }
+                        SYSCALL_FIRED = 0;
+                    }
+
+                }
+            },
+            None => {},
+        }
+        unsafe { 
+            if SYSTICK_FIRED > 0 {
+                asm!("cpsid i" ::: "memory" : "volatile");
+                if self.procs.is_some() {
+                    let current = self.procs.take().unwrap();
+                    let next = current.next.take();
+                    if next.is_some() {
+                        let current_ptr = current as *mut ProcessList;
+                        let mut last = self.last.take();
+                        match last.iter().next() {
+                            Some(item) => {
+                                (*(*item)).next.replace(current);
+                            },
+                            None => {},
+                        };
+                        self.procs = next;
+                        self.last.replace(current_ptr);
+                    } else {
+                        self.procs.replace(current);
+                    }
+                    
+                }
+                SYSTICK_FIRED = 0;
+                asm!("cpsie i" ::: "memory" : "volatile");
+            }
+        }
+
+        if nvic.is_pending(IrqId::USART3 as u32) {
+            serial.read().map(|c| serial.write(c));
+            nvic.clear_pending(IrqId::USART3 as u32);
+            nvic.enable(IrqId::USART3 as u32);
+        }
+
+    }
+}
+
 pub fn main() -> ! {
     //let hstdout = hio::hstdout().unwrap();
     //let mut logger = Logger { hstdout };
@@ -62,8 +171,8 @@ pub fn main() -> ! {
 
     //logger.log(address as u8);
     
-    let mut process = process_create!(app_main, 1024);
-    let mut tick_process = process_create!(tick, 512);
+    let process = process_create!(app_main, 1024);
+    let tick_process = process_create!(tick, 512);
     let registers = RCC.get_registers_ref();
 
     unsafe {
@@ -83,13 +192,23 @@ pub fn main() -> ! {
         gpiob.get_registers_ref().moder.modify(|val| (val | 0x1 | (0x1 << 14)));
         gpiob.get_registers_ref().bsrr.write(0x1);
     }
-    let nvic = Nvic::new();
+    let mut nvic = Nvic::new();
     nvic.enable(IrqId::USART3 as u32);
     for c in "hello world".chars() {
         serial.write(c);
     }
     // Dummy code to prevent optimizing
     unsafe { IRQS[0] = Vector { reserved: 0 }; }
+    let mut scheduler = Scheduler::create();
+    let mut proc_item = ProcessList::create(process);
+    let mut tick_item = ProcessList::create(tick_process);
+    scheduler.push(&mut proc_item);
+    scheduler.push(&mut tick_item);
+
+    loop {
+        scheduler.run(&mut serial, &mut nvic);
+    }
+    /*
     loop {
         process.execute();
         unsafe {
@@ -122,6 +241,7 @@ pub fn main() -> ! {
             nvic.enable(IrqId::USART3 as u32);
         }
     }
+    */
 
     // debug!(logger, "Goodbye");
     //dhprintln!("goodbye");
