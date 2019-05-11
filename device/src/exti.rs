654a17d923f2ae7b3b0468ff84046956833ab280
garasubo <garasubo@gmail.com>
2019-05-11 22:42:45 +0900

    add gpio interrupt

---
+++ device/src/exti.rs
@@ -0,0 +1,32 @@
+use volatile_register::RW;
+use core::ops::Deref;
+
+#[repr(C)]
+pub struct ExtiRegisters {
+    pub imr: RW<u32>,
+    pub emr: RW<u32>,
+    pub rtsr: RW<u32>,
+    pub ftsr: RW<u32>,
+    pub swier: RW<u32>,
+    pub pr: RW<u32>,
+}
+
+pub struct Exti {
+    base: u32,
+}
+
+impl Deref for Exti {
+    type Target = ExtiRegisters;
+
+    fn deref(&self) -> &Self::Target {
+        let registers = self.base as *mut ExtiRegisters;
+        unsafe { &*registers }
+    }
+}
+
+
+impl Exti {
+    pub const fn new(base: u32) -> Exti {
+        Exti { base }
+    }
+}
---
+++ device/src/syscfg.rs
@@ -0,0 +1,34 @@
+use volatile_register::RW;
+use core::ops::Deref;
+
+#[repr(C)]
+pub struct SyscfgRegisters {
+    pub memrmp: RW<u32>,
+    pub pmc: RW<u32>,
+    pub exticr1: RW<u32>,
+    pub exticr2: RW<u32>,
+    pub exticr3: RW<u32>,
+    pub exticr4: RW<u32>,
+    _reserved: u32,
+    pub cmpcr: RW<u32>,
+}
+
+pub struct Syscfg {
+    base: u32,
+}
+
+impl Deref for Syscfg {
+    type Target = SyscfgRegisters;
+
+    fn deref(&self) -> &Self::Target {
+        let registers = self.base as *mut SyscfgRegisters;
+        unsafe { &*registers }
+    }
+}
+
+
+impl Syscfg {
+    pub const fn new(base: u32) -> Syscfg {
+        Syscfg { base }
+    }
+}
