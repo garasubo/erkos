0d5b4e8444819e2827479facb5e656e3f4066bc4
garasubo <garasubo@gmail.com>
2021-05-03 20:20:26 +0900

    implment led driver

---
+++ devices/wio_terminal/build.rs
@@ -0,0 +1,12 @@
+use std::{env, error::Error, fs::File, io::Write, path::PathBuf};
+
+fn main() -> Result<(), Box<dyn Error>> {
+    // Put the linker script somewhere the linker can find it
+    let out = PathBuf::from(env::var("OUT_DIR")?);
+
+    File::create(out.join("device.ld"))?.write_all(include_bytes!("device.ld"))?;
+
+    println!("cargo:rustc-link-search={}", out.display());
+
+    Ok(())
+}
--- devices/wio_terminal/src/lib.rs
+++ devices/wio_terminal/src/lib.rs
@@ -1,2 +1,5 @@
 #![no_std]
 #![feature(asm)]
+
+pub mod port;
+pub mod led;
